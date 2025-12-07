cdcf53cc10901527066cadac8d164927a6f54a56
Ryan Weavers <ryanweavers@gmail.com>
2025-12-07 21:05:21 +0000

    Minor tweaks

--- CSharpPlayersGuidePractice/Program.cs
+++ CSharpPlayersGuidePractice/Program.cs
@@ -4,6 +4,6 @@ Console.WriteLine("How would you describe it? Big? Azure? Tattered?");
 string descriptor = Console.ReadLine();
 Console.WriteLine("What kind of thing are you talking about?");
 string thing = Console.ReadLine(); // User  the object
-string doom = ("of DOOM!"); // Doom!
+string doom = (" of DOOM"); // Doom!
 string model = ("3000"); // 3000!
-Console.WriteLine("The" + descriptor +" " + thing + " of " + doom + " " + model + "!"); // Concatenated string
+Console.WriteLine("The " + descriptor + " " + thing + doom + " " + model + "!"); // Concatenated string
