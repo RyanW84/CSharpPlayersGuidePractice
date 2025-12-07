d21611e27ebde0f2e5c4735c1aa3462918808c16
Ryan Weavers <ryanweavers@gmail.com>
2025-12-07 21:08:04 +0000

    Answered the question

--- CSharpPlayersGuidePractice/Program.cs
+++ CSharpPlayersGuidePractice/Program.cs
@@ -1,9 +1,12 @@
 ﻿
 
 Console.WriteLine("How would you describe it? Big? Azure? Tattered?");
-string descriptor = Console.ReadLine();
+string? descriptor = Console.ReadLine();
 Console.WriteLine("What kind of thing are you talking about?");
-string thing = Console.ReadLine(); // User  the object
+string? thing = Console.ReadLine(); // User  the object
 string doom = (" of DOOM"); // Doom!
 string model = ("3000"); // 3000!
 Console.WriteLine("The " + descriptor + " " + thing + doom + " " + model + "!"); // Concatenated string
+
+// What other improvements could me made?
+// Choosing variable names and using interpolation
