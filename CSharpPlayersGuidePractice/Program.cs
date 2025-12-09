c467b066d1e2ffd6c1faec8a5062708b57788397
Ryan Weavers <ryanweavers@gmail.com>
2025-12-09 20:45:26 +0000

    Challenge The Triangle Farmer

--- CSharpPlayersGuidePractice/Program.cs
+++ CSharpPlayersGuidePractice/Program.cs
@@ -1,69 +1,13 @@
-﻿// The Variable Shop
+﻿// Challenge: The Triangle farmer
 
-// Initialise the variables
-string words = "Hello World";
-char character = 'a';
-byte byteExample = 1;       // Number of bytes
-ushort ushortExample = 2;   // Number of bytes
-uint uintExample = 4;       // Number of bytes
-ulong ulongExample = 8;     // Number of bytes
-short shortExample = 2;
-int intExample = 4;
-long longExample = 8;
-float floatExample = 12.5F; // Note the F
-double doubleExample = 12.52425D; // Note the D
-bool exampleBool = true;
-int binaryLiteral = 0b00001101;
-int hexLiteral = 0xFF00FF;
+using System.Globalization;
 
-// Print the values
-Console.WriteLine("First Values:");
-Console.WriteLine(words);
-Console.WriteLine(character);
-Console.WriteLine(byteExample);
-Console.WriteLine(ushortExample);
-Console.WriteLine(uintExample);
-Console.WriteLine(ulongExample);
-Console.WriteLine(shortExample);
-Console.WriteLine(intExample);
-Console.WriteLine(longExample);
-Console.WriteLine(floatExample);
-Console.WriteLine(doubleExample);
-Console.WriteLine(exampleBool);
-Console.WriteLine(binaryLiteral);
-Console.WriteLine(hexLiteral);
+Console.Write("Enter Triangle base: ");
+string? baseTriangle = Console.ReadLine();
+float baseTriangleFloat = float.Parse(baseTriangle!, CultureInfo.InvariantCulture);
+Console.Write("Enter Triangle height: ");
+string? heightTriangle = Console.ReadLine();
+float heightTriangleFloat = float.Parse(heightTriangle!, CultureInfo.InvariantCulture);
+float result = (baseTriangleFloat * heightTriangleFloat) / 2f;
 
-
-// Assign new values
-words = "The Uncoded One Returns";
-character = 'b';
-byteExample = 3;
-ushortExample = 4;
-uintExample = 5;
-ulongExample = 6;
-shortExample = 7;
-intExample = 8;
-longExample = 9;
-floatExample = 10.5F;
-doubleExample = 11.242D;
-exampleBool = false;
-binaryLiteral = 0b00000110;
-hexLiteral = 0xEE33EE;
-
-// Print new values
-Console.WriteLine("");
-Console.WriteLine("New Values:");
-Console.WriteLine(words);
-Console.WriteLine(character);
-Console.WriteLine(byteExample);
-Console.WriteLine(ushortExample);
-Console.WriteLine(uintExample);
-Console.WriteLine(ulongExample);
-Console.WriteLine(shortExample);
-Console.WriteLine(intExample);
-Console.WriteLine(longExample);
-Console.WriteLine(floatExample);
-Console.WriteLine(doubleExample);
-Console.WriteLine(exampleBool);
-Console.WriteLine(binaryLiteral);
-Console.WriteLine(hexLiteral);
+Console.WriteLine("The result is : " + result);
