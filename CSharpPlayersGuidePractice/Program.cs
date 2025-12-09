07c218f714c04716ba020f2b3f713f09fed73a0f
Ryan Weavers <ryanweavers@gmail.com>
2025-12-09 13:51:58 +0000

    Challenge: The Variable Shop

--- CSharpPlayersGuidePractice/Program.cs
+++ CSharpPlayersGuidePractice/Program.cs
@@ -1,10 +1,34 @@
-﻿Console.WriteLine("How would you describe it? Big? Azure? Tattered?");
-string? descriptor = Console.ReadLine();
-Console.WriteLine("What kind of thing are you talking about?");
-string? thing = Console.ReadLine(); // User  the object
-string doom = (" of DOOM"); // Doom!
-string model = ("3000"); // 3000!
-Console.WriteLine("The " + descriptor + " " + thing + doom + " " + model + "!"); // Concatenated string
+﻿// The Variable Shop
 
-// What other improvements could be made?
-// Choosing variable names and using interpolation
+string words = "Hello World";
+char character = 'a';
+byte byteExample = 1;       // Number of bytes
+ushort ushortExample = 2;   // Number of bytes
+uint uintExample = 4;       // Number of byters
+ulong ulongExample = 8;     // Number of bytes
+short shortExample = 2;
+int intExample = 4;
+long longExample = 8;
+float floatExample = 12.5F; // Note the F
+double doubleExample = 12.5D; // Note the D
+bool exampleBool = true;
+int binaryLiteral = 0b00001101;
+int hexLiteral = 0xFF00FF;
+
+
+
+
+Console.WriteLine(words);
+Console.WriteLine(character);
+Console.WriteLine(byteExample);
+Console.WriteLine(ushortExample);
+Console.WriteLine(uintExample);
+Console.WriteLine(ulongExample);
+Console.WriteLine(shortExample);
+Console.WriteLine(intExample);
+Console.WriteLine(longExample);
+Console.WriteLine(floatExample);
+Console.WriteLine(doubleExample);
+Console.WriteLine(exampleBool);
+Console.WriteLine(binaryLiteral);
+Console.WriteLine(hexLiteral);
