c36963aa59586dd698beb30387b04d5788c735b9
Ryan Weavers <ryanweavers@gmail.com>
2025-12-11 20:20:45 +0000

    Improvements made

--- CSharpPlayersGuidePractice/Program.cs
+++ CSharpPlayersGuidePractice/Program.cs
@@ -2,24 +2,37 @@
 
 Console.BackgroundColor = ConsoleColor.Black;
 Console.ForegroundColor = ConsoleColor.Yellow;
+Console.Title = " The Dominion of Kings";
 
-Console.WriteLine("Enter the number of Provinces");
-int provinces = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Enter the number of Duchies");
-int duchies = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Enter the number of Estates");
-int estates = Convert.ToInt32(Console.ReadLine());
-int result = 0;
-Console.WriteLine("");
+char tryAgain = 'y';
 
-provinces = provinces*=6;
-Console.WriteLine($"The total Provinces score is {provinces}");
-duchies = duchies*=3;
-Console.WriteLine($"The total Duchies score is {duchies}");
-estates = estates*=1;
-Console.WriteLine($"The total Estates score is {estates}");
+while (tryAgain is 'y' or 'Y')
+{
+    Console.WriteLine("Enter the number of Provinces");
+    var provinces = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Enter the number of Duchies");
+    var duchies = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Enter the number of Estates");
+    var estates = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("");
 
-result = provinces+duchies+estates;
+    provinces = provinces *= 6;
+    Console.WriteLine($"The total Provinces score is {provinces}");
+    duchies = duchies *= 3;
+    Console.WriteLine($"The total Duchies score is {duchies}");
+    estates = estates *= 1;
+    Console.WriteLine($"The total Estates score is {estates}");
 
-Console.WriteLine();
-Console.WriteLine($"The total is {result}");
+    var result = provinces + duchies + estates;
+
+    Console.WriteLine();
+    Console.WriteLine($"The total is {result}");
+
+    tryAgain = ' ';
+    
+    Console.WriteLine("Press Y to try again, or any other key to exit");
+    tryAgain = Console.ReadKey().KeyChar;
+    
+}
+
+Console.WriteLine("\n\nGoodbye");
