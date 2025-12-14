0f29594a028f8eb1b875da359a064f6632c822da
Ryan Weavers <ryanweavers@gmail.com>
2025-12-14 20:12:14 +0000

    Complete

--- CSharpPlayersGuidePractice/Program.cs
+++ CSharpPlayersGuidePractice/Program.cs
@@ -6,9 +6,32 @@ Console.BackgroundColor = ConsoleColor.Black;
 Console.ForegroundColor = ConsoleColor.Yellow;
 Console.Title = " The Defence of Consolas";
 
+Console.WriteLine("Enter the target row");
+int targetRow = int.Parse((Console.ReadLine()));
+Console.WriteLine("Enter the target column");
+int targetColumn = int.Parse((Console.ReadLine()));
+
+int topRow = targetRow;
+int topColumn = targetColumn + 1;
+
+int bottomRow = targetRow - 1;
+int bottomColumn = targetColumn;
+
+int leftRow = targetRow;
+int leftColumn = targetColumn + 1;
+
+int rightRow = targetRow+1;
+int rightColumn = targetColumn;
 
 
 
+Console.WriteLine("The Defending coordinates are:");
+Console.WriteLine($"Top: {topRow}, {topColumn}");
+Console.WriteLine($"Bottom: {bottomRow}, {bottomColumn}");
+Console.WriteLine($"Left: {leftRow}, {leftColumn}");
+Console.WriteLine($"Right: {rightRow}, {rightColumn}");
+
+Console.WriteLine("Beep");
 Console.Beep(264, 125);
 
 
