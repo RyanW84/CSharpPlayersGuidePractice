010301d98662cca5dbc80d715f379cf597718bb1
Ryan Weavers <ryanweavers@gmail.com>
2025-12-15 20:32:16 +0000

    Customised

--- CSharpPlayersGuidePractice/Program.cs
+++ CSharpPlayersGuidePractice/Program.cs
@@ -1,4 +1,16 @@
 ﻿// Repairing the Clocktower
+Console.BackgroundColor = ConsoleColor.Red;
+Console.ForegroundColor = ConsoleColor.White;
+Console.Title = "Repairing the Clocktower";
 
+Console.WriteLine("Enter the hour");
 int userInput = Int32.Parse(Console.ReadLine());
 
+if (userInput % 2 == 0)
+{
+    Console.WriteLine("Tick");
+}
+else
+{
+    Console.WriteLine("Tock");
+}
