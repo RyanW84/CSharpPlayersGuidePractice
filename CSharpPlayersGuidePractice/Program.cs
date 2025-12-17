44d16a01b8bc8c9831a17655a9ac3a37f8785424
Ryan Weavers <ryanweavers@gmail.com>
2025-12-17 20:27:57 +0000

    Finished challenge

--- CSharpPlayersGuidePractice/Program.cs
+++ CSharpPlayersGuidePractice/Program.cs
@@ -1,8 +1,47 @@
 ﻿// Challenge: Buying Inventory
 
 Console.Title = "Buying Inventory";
-Console.BackgroundColor = ConsoleColor.Yellow;
-Console.ForegroundColor = ConsoleColor.Black;
+Console.BackgroundColor = ConsoleColor.Black;
+Console.ForegroundColor = ConsoleColor.Yellow;
 Console.Clear(); // Ensures clean display with new colors
 
-Console.WriteLine("=== Buying Inventory ===");
+Console.WriteLine("=== Buying Inventory ===\n\n");
+
+Console.WriteLine(@"The following items are available:
+
+1 - Rope
+2 - Torches
+3 - Climbing Equipment
+4 - Clean Water
+5 - Machete
+6 - Canoe
+7 - Food Supplies
+
+Which number do you want to see the price of? ");
+
+int input = int.Parse(Console.ReadLine());
+
+switch (input)
+{
+    case 1:
+        Console.WriteLine("Rope: 10 Gold");
+        break;
+    case 2:
+        Console.WriteLine("Torches: 15 Gold");
+        break;
+    case 3:
+        Console.WriteLine("Climbing Equipment: 25 Gold");
+        break;
+    case 4:
+        Console.WriteLine("Clean Water: 1 Gold");
+        break;
+    case 5:
+        Console.WriteLine("Machete: 20 Gold");
+        break;
+    case 6:
+        Console.WriteLine("Canoe: 200 Gold");
+        break;
+    case 7:
+        Console.WriteLine("Food Supplies: 1 Gold");
+        break;
+}
