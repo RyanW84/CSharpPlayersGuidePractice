889b498f703034e8a4b3d232893bfff7cf949458
Ryan Weavers <ryanweavers@gmail.com>
2025-12-21 20:07:04 +0000

    Challenge: The Prototype

--- CSharpPlayersGuidePractice/Program.cs
+++ CSharpPlayersGuidePractice/Program.cs
@@ -1,68 +1,39 @@
-﻿// Challenge: Discounted Inventory
+﻿// Challenge: The Prototype
 
 Console.Title = "Discounted Inventory";
 Console.BackgroundColor = ConsoleColor.Black;
 Console.ForegroundColor = ConsoleColor.Yellow;
 Console.Clear(); // Ensures clean display with new colors
 
-Console.WriteLine("=== Discounted Inventory ===\n\n");
+Console.WriteLine("=== The Prototype ===\n\n");
 
-Console.WriteLine("What is your name!!!");
-string? name = Console.ReadLine();
+bool continueGame = True;
 
-Console.WriteLine(@"The following items are available:
+Console.WriteLine("Pilot enter your number (0-100)");
+int pilot = Int32.Parse(Console.ReadLine() ?? string.Empty);
+Console.Clear();
 
-1 - Rope
-2 - Torches
-3 - Climbing Equipment
-4 - Clean Water
-5 - Machete
-6 - Canoe
-7 - Food Supplies
+Console.WriteLine("Hunter enter your guess (0-100)");
+int guess = Int32.Parse(Console.ReadLine() ?? string.Empty);
 
-Which number do you want to see the price of? ");
-
-int input = int.Parse(Console.ReadLine() ?? string.Empty);
-float value;
-
-// Switch usage
-switch (input)
+while (continueGame)
 {
-    case 1:
-        value = 10;
-        if (name?.ToLower() == "ryan") value /= 2;        
-        Console.WriteLine($"Rope: {value} Gold");
+    if (guess < pilot)
+    {
+        Console.WriteLine("Too low");
+    }
+    else if (guess > pilot)
+    {
+        Console.WriteLine("Too high");
+    }
+    else if (guess == pilot)
+    {
+        continueGame = false;
+        Console.WriteLine("You Win!");
         break;
-    case 2:
-        value = 15;
-        if (name?.ToLower() == "ryan") value /= 2;        
-        Console.WriteLine($"Torches: {value} Gold");
-        break;
-    case 3:
-        value = 25;
-        if (name?.ToLower() == "ryan") value /= 2;        
-        Console.WriteLine($"Climbing Equipment: {value} Gold");
-        break;
-    case 4:
-        value =1;
-        if (name?.ToLower() == "ryan") value /= 2;        
-        Console.WriteLine($"Clean Water: {value} Gold");
-        break;
-    case 5:
-        value = 20;
-        if (name?.ToLower() == "ryan") value /= 2;
-        Console.WriteLine($"Machete: {value} Gold");
-        break;
-    case 6:
-        value = 200;
-        if (name?.ToLower() == "ryan") value /= 2;
-        Console.WriteLine($"Canoe: {value} Gold");
-        break;
-    case 7:
-        value = 1;
-        if (name?.ToLower() == "ryan") value /= 2;
-        Console.WriteLine($"Food Supplies: {value} Gold");
-        break;
-    
-    // Saving my streak
+    }
+
+    Console.WriteLine("Try again Hunter");    
+    guess = Int32.Parse(Console.ReadLine() ?? string.Empty);
 }
+
